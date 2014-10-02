455ca20647b8df7f7dbb561bdad65e8bbc82bf69
Delphine <delphine_trahand@yahoo.com>
2014-10-02 11:18:00 +0200

    footer formatting not working on server (but ok locally). tried changing the class name. Perhaps 'footer' is overtaken, changed it to MyFooter.

--- footerfile.php
+++ footerfile.php
@@ -1,4 +1,4 @@
-<div class="footer">
+<div class="MyFooter">
 <hr>
 Class: Dynamic Web Applications CSCI E-15 - Fall 2014<br />
 Assignment: <a href="http://dwa15.com/Projects/P2">Project2</a><br />
